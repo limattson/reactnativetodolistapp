989bbf49e8ad9cf30826500d311debc0e26b8434
Li Mattson <liamattson@gmail.com>
2023-12-21 18:39:11 -0700

    last react components adapted

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import '../App.css';
-
+import { View, Text, StyleSheet } from 'react-native';
 
 const Header = () => {
     return (
-        <header className='app-header'>
-            <h1 className='heading-one'>Off Your Mind</h1>
-            <h4 className='heading-four'>Less remembering more doing</h4>
-        </header>
+        <View style={styles.appHeader}>
+            <Text style={styles.headingOne}>Off Your Mind</Text>
+            <Text style={styles.headingFour}>Less remembering more doing</Text>
+        </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    appHeader: {
+        backgroundColor: '#b3926c',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+        color: '#efebe2',
+        fontFamily: 'League Spartan, sans-serif',
+        borderRadius: 18,
+    },
+    headingOne: {
+        fontFamily: 'League Spartan',
+        textTransform: 'uppercase',
+        justifyContent: 'center',
+        textAlign: 'center',
+        display: 'flex', // or 'block', depending on the use case
+    },
+    headingFour: {
+        fontFamily: 'Libre Baskerville',
+        fontStyle: 'italic',
+        justifyContent: 'center',
+        textAlign: 'center',
+        display: 'flex', // or 'block', depending on the use case
+    },
+});
 
 export default Header;
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,54 @@
-const ListItem = ({item, dispatch}) => {
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const ListItem = ({ item, dispatch }) => {
+    const handleRemoveItem = () => {
+        dispatch({ type: 'REMOVE_ITEM', payload: item });
+    };
+
     return (
-        <div className='list-item'>
-            <button className='remove-list-item' onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: item})}>
-            ✓
-            </button>
-            <p className='item-text'>{item.name}</p>
-        </div>
+        <View style={styles.listItem}>
+            <TouchableOpacity
+                style={styles.removeListItem}
+                onPress={handleRemoveItem}
+            >
+                <Text style={styles.removeText}>✓</Text>
+            </TouchableOpacity>
+            <Text style={styles.itemText}>{item.name}</Text>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    listItem: {
+        fontFamily: 'Libre Baskerville, sans-serif',
+        fontStyle: 'italic',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 5,
+        paddingRight: 5,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        marginTop: 2,
+        marginBottom: 2,
+        marginLeft: 3,
+        marginRight: 3,
+    },
+    removeListItem: {
+        backgroundColor: 'red',
+        padding: 8,
+        borderRadius: 4,
+        marginRight: 8,
+    },
+    removeText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    itemText: {
+        fontSize: 16,
+    },
+});
+
 export default ListItem;
